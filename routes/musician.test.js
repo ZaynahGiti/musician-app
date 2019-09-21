2cd2cb5c8e58e82cb41cf7fdfb72f111b2c35199
Mac <mac@macs-mbp.mynet>
2019-09-21 11:50:40 +0300

    error

--- routes/musician.test.js
+++ routes/musician.test.js
@@ -1,4 +1,4 @@
-const request = require('supertest');
+//const request = require('supertest');
 const store = require('../store/datastore');
 const initialStoreData = require('../store/data');
 const musicianRoutes = require('./musician');
